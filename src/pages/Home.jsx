f1d32389bba4009324a5a89504d6eabcbef61806
RohanAM6606 <rohan02aug@gmail.com>
2026-03-26 01:00:41 +0530

    added domians (Aryan Raj)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import HeroSection from "../components/Hero/HeroSection";
 import Sponsor from "../components/Sponsor/Sponsor";
 import Navbar from "../components/Navbar/Navbar";
 import EventDetails from "../components/EventDetails/EventDetails";
-
+import Domain from "../components/Domain/Domain";
 const Home = () => {
   return (
     <div>
       <div data-section="home">
         <HeroSection />
+      </div>
+
+      <div data-section="domain">
+        <Domain />
       </div>
       <div data-section="eventdetails">
         <EventDetails />
